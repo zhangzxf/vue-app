3b68d1d20da54f422a28608e101ce9020ad171d5
zxf <zxf@qq.com>
2019-01-02 21:28:28 +0800

    这是一个高端项目

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="app-container">
+
+    <!--  顶部 Header 区域  -->
+    <mt-header fixed title=" hello world"></mt-header>
+    <!-- 中间的路由 router-view 区域 -->
+
+    <!-- 底部 Tabbar 区域 -->
+        <mt-tabbar :selected.sync="selected">
+      <mt-tab-item id="首页">
+        <img slot="icon" src="">
+        首页
+      </mt-tab-item>
+      <mt-tab-item id="会员">
+        <img slot="icon" src="">
+        会员
+      </mt-tab-item>
+      <mt-tab-item id="购物车">
+        <img slot="icon" src="">
+        购物车
+      </mt-tab-item>
+      <mt-tab-item id="搜索">
+        <img slot="icon" src="">
+        搜索
+      </mt-tab-item>
+    </mt-tabbar>
+
+<h1>123</h1>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="less" scoped>
+  .app-container{
+    margin-top:40px;
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,20 @@
+// 入口文件
+import Vue from 'vue'
+
+// 按需导入 Mint-UI 中的组件
+// import { Header } from 'mint-ui';
+
+// Vue.component(Header.name, Header);
+ import MintUI from 'mint-ui'
+import "mint-ui/lib/style.css"
+
+Vue.use(MintUI)
+
+// 导入 App 根组件
+import app from './App'
+
+
+var vm = new Vue({
+  el:'#app',
+  render: c => c(app)
+})
